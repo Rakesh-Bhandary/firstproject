b18640543d40a69613b8d6c404af59b617dedc30
rakesh <rakesh456bhandary@gmail.com>
2024-12-06 00:22:24 +0530

    first commit

---
+++ src/firstseleniumproject/invokebrowser.java
@@ -0,0 +1,17 @@
+package firstseleniumproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class invokebrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.verizon.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+
+	}
+
+}
